865ba71e84e403e0e648c859a0930623b62576a0
M. Ihsan Rizqullah Adfa <159456948+ihsanadfa@users.noreply.github.com>
2024-11-20 23:13:33 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/afrizal423/Golang-Perpustakaan-Restful-API/api"
-
 	userController "github.com/afrizal423/Golang-Perpustakaan-Restful-API/api/v1/user"
 	userService "github.com/afrizal423/Golang-Perpustakaan-Restful-API/app/business/user"
 	userRepository "github.com/afrizal423/Golang-Perpustakaan-Restful-API/app/repository/user"
@@ -13,30 +12,30 @@ import (
 
 	"github.com/afrizal423/Golang-Perpustakaan-Restful-API/configs"
 	"github.com/gofiber/fiber/v2"
+	"log"
 )
 
 func main() {
+	// Koneksi ke database MySQL
 	db := configs.MySQLConn()
-	// database.Migrate()
-	// database.Seeder(db)
 
+	// Membuat repository dan service untuk user
 	userRepo := userRepository.NewUserRepository(db)
 	userServices := userService.NewUserService(userRepo)
 	userCon := userController.NewUserController(userServices)
 
+	// Membuat repository dan service untuk buku
 	bukuRepo := bukuRepository.NewBukuRepository(db)
 	bukuServices := bukuService.NewBukuService(bukuRepo)
 	bukuCon := bukuController.NewBukuController(bukuServices)
 
+	// Konfigurasi server Fiber
 	config := configs.ServerTimeOut()
 	app := fiber.New(config)
 
-	api.RegisterPath(
-		app,
-		userCon,
-		bukuCon,
-	)
+	// Menambahkan path API untuk user dan buku
+	api.RegisterPath(app, userCon, bukuCon)
 
-	app.Listen(":8080")
-
+	// Menjalankan aplikasi di port 8080
+	log.Fatal(app.Listen(":8080"))
 }
